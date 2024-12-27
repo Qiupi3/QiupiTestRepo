1334335eed1274c378c42f0f6f9180021ccd99b5
Qiupi <trimas.putra7@gmail.com>
2024-12-27 20:28:57 +0700

    lalala

--- scripts/global.js
+++ scripts/global.js
@@ -5,14 +5,6 @@ var TrainerData = JSON.parse(localStorage.getItem("Trainer"));
 var MovesData = JSON.parse(localStorage.getItem("Move"));
 var ItemsData = JSON.parse(localStorage.getItem("Items"));
 var LearnsetData = JSON.parse(localStorage.getItem("Learnset"));
-//var SpriteData = JSON.parse(localStorage.getItem("Sprite"));
-
-async function ReqSpriteData() {
-    const resp = await fetch("data/Sprite.json");
-    const content = await resp.json();
-    window.localStorage.setItem("Sprite", JSON.stringify(content));
-    //window.location = window.location;
-}
 
 async function ReqAbilityData() {
     const resp = await fetch("data/Ability.json");
@@ -53,7 +45,6 @@ async function ReqLearnsetData() {
 
 
 if (!(AbilityData && SpeciesData && LocationData && ItemsData && LearnsetData && MovesData)) {
-    //ReqSpriteData();
     ReqSpeciesData();
     ReqAbilityData();
     ReqLocationData();
@@ -62,20 +53,3 @@ if (!(AbilityData && SpeciesData && LocationData && ItemsData && LearnsetData &&
     ReqItemsData();
     ReqLearnsetData();
 }
-
-/*async function ReqData() {
-    const resp = await fetch("data/data.json");
-    const content = await resp.json();
-    console.log(content)
-    window.localStorage.setItem("Ability", JSON.stringify(content[0]));
-    window.localStorage.setItem("Species", JSON.stringify(content[1]));
-    window.localStorage.setItem("Location", JSON.stringify(content[2]));
-    window.localStorage.setItem("Move", JSON.stringify(content[3]));
-    window.localStorage.setItem("Items", JSON.stringify(content[4]));
-    window.localStorage.setItem("Learnset", JSON.stringify(content[5]));
-    window.localStorage.setItem("Sprite", JSON.stringify(content[6]));
-}
-
-if (!(AbilityData && SpeciesData && LocationData && ItemsData && LearnsetData && MovesData && SpriteData)) {
-    ReqData();
-}*/
