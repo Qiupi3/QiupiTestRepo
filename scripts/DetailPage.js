89886182c29cc257faa4e96846499011bfbae202
Qiupi <trimas.putra7@gmail.com>
2025-02-11 22:14:41 +0700

    up

--- scripts/DetailPage.js
+++ scripts/DetailPage.js
@@ -8,186 +8,210 @@ function OpenDetails(SpeciesID) {
     let tab = document.getElementById("speciesDetail");
     tab.className = '';
 
-    const SpeciesData = JSON.parse(localStorage.getItem("Species"));
-    const Species = SpeciesData[SpeciesID-1];
+    // const SpeciesData = JSON.parse(localStorage.getItem("Species"));
+    const species = SpeciesData[SpeciesID-1];
     
-    const UID = document.getElementById("UIDDetail");
-    const Num = document.getElementById("NumDetail");
-    const Sprite = document.getElementById("SpriteDetail");
-    const Type = document.getElementById("TypeDetail");
-    const Ability = document.getElementById("AbilityDetail");
-    const Location = document.getElementById("LocationDetail");
-    const HeldItem = document.getElementById("HeldItemDetail");
-    const EggGroup = document.getElementById("EggGroupDetail");
-    const Evolution = document.getElementById("EvoDetail");
+    const container = 
+    `
+    <div class="headContainer">
+        <div id="closeDetail" class="headButton" onclick="CloseDetails(true)">
+            &times
+        </div>
+        <h1>
+            RR Dex - Details
+        </h1>
+        <div id="prevDetail" class="headButton" onclick="PrevDetails()">
+            «
+        </div>
+        <div id="nextDetail" class="headButton" onclick="NextDetails()">
+            »
+        </div>
+    </div>
+    <div class="generalContainer" id="${species.UID}">
+        <div>#${species.SID} ${species.Name}</div>
+        <div><img src="Assets/Transparent/${species.SUID}.png"></div>
+        <div>Type :${TypeBox(species.Type).innerHTML}</div>
+        <div>Ability :${AbilityBox(species.Ability, true).innerHTML}</div>
+    </div>
+    `
+    tab.innerHTML = container;
+    // const UID = document.getElementById("UIDDetail");
+    // const Num = document.getElementById("NumDetail");
+    // const Sprite = document.getElementById("SpriteDetail");
+    // const Type = document.getElementById("TypeDetail");
+    // const Ability = document.getElementById("AbilityDetail");
+    // const Location = document.getElementById("LocationDetail");
+    // const HeldItem = document.getElementById("HeldItemDetail");
+    // const EggGroup = document.getElementById("EggGroupDetail");
+    // const Evolution = document.getElementById("EvoDetail");
     
-    let SpriteImage = `<img src="Assets/Transparent/${Species.SUID}.png">`
+    // let SpriteImage = `<img src="Assets/Transparent/${Species.SUID}.png">`
 
-    UID.innerText = Species.UID;
-    Num.innerText = `#${Species.SID} ${Species.Name}`;
-    Sprite.innerHTML = "";
-    Sprite.innerHTML += SpriteImage;
-    Type.innerHTML = "Type :" + TypeBox(Species.Type).innerHTML
-    Ability.innerHTML = "Ability :" + AbilityBox(Species.Ability, true).innerHTML;
+    // UID.innerText = Species.UID;
+    // Num.innerText = `#${Species.SID} ${Species.Name}`;
+    // Sprite.innerHTML = "";
+    // Sprite.innerHTML += SpriteImage;
+    // Type.innerHTML = "Type :" + TypeBox(Species.Type).innerHTML
+    // Ability.innerHTML = "Ability :" + AbilityBox(Species.Ability, true).innerHTML;
     
-    const statsTable = document.getElementById("statsTable").rows;
-    for (let x = 0; x < statsTable.length; x++) {
-        const StatsNumber = Object.values(Species)[6 + x];
-        const cell = statsTable[x].getElementsByTagName("td");
-        cell[0].innerText = StatsNumber;
-    }
+    // const statsTable = document.getElementById("statsTable").rows;
+    // for (let x = 0; x < statsTable.length; x++) {
+    //     const StatsNumber = Object.values(Species)[6 + x];
+    //     const cell = statsTable[x].getElementsByTagName("td");
+    //     cell[0].innerText = StatsNumber;
+    // }
     
-    for (let x = 0; x < statsTable.length; x++) {
-        const cell = statsTable[x].getElementsByTagName("td");
-        const box = cell[1].getElementsByTagName("div")[0];
-        box.style.color = "transparent";
-        if (x == 0) {
-            box.style.width = (5 + cell[0].innerText/8) + "%";
-            box.style.backgroundColor = "hsl(" + cell[0].innerText/7 + ", 85%, 45%)";
-        } else {
-            box.style.width = (5 + cell[0].innerText/2) + "%";
-            box.style.backgroundColor = "hsl(" + cell[0].innerText/17*10 + ", 95%, 65%)";
-        }
-    }
+    // for (let x = 0; x < statsTable.length; x++) {
+    //     const cell = statsTable[x].getElementsByTagName("td");
+    //     const box = cell[1].getElementsByTagName("div")[0];
+    //     box.style.color = "transparent";
+    //     if (x == 0) {
+    //         box.style.width = (5 + cell[0].innerText/8) + "%";
+    //         box.style.backgroundColor = "hsl(" + cell[0].innerText/7 + ", 85%, 45%)";
+    //     } else {
+    //         box.style.width = (5 + cell[0].innerText/2) + "%";
+    //         box.style.backgroundColor = "hsl(" + cell[0].innerText/17*10 + ", 95%, 65%)";
+    //     }
+    // }
     
-    const SpeciesLocation = splitFunc(Species.Location);
-    Location.innerText = "Location : \n";
-    if (SpeciesLocation != "-") {
-        for (let x = 0; x < SpeciesLocation.length; x++) {
-            let LocIndex = SpeciesLocation[x] - 30001;
-            let LocationObj = LocationData[LocIndex];
-            Location.innerText += "• " + LocationObj.Loc + " " + LocationObj.Res + "\n";
-            let LocSUIDArr = splitFunc(LocationObj.SUID);
-            let LocERArr = splitFunc(LocationObj.ER);
-            let LocLvArr = splitFunc(LocationObj.Lv);
-            for (let x = 0; x < LocSUIDArr.length; x++) {
-                if (LocSUIDArr[x] == Species.SUID) {
-                    Location.innerText += "Lv : " + LocLvArr[x] + " " + LocERArr[x] + "\n";
-                }
-            }
-        }
-    } else {
-        Location.innerText += `• Unknown`;
-    }
+    // const SpeciesLocation = splitFunc(Species.Location);
+    // Location.innerText = "Location : \n";
+    // if (SpeciesLocation != "-") {
+    //     for (let x = 0; x < SpeciesLocation.length; x++) {
+    //         let LocIndex = SpeciesLocation[x] - 30001;
+    //         let LocationObj = LocationData[LocIndex];
+    //         Location.innerText += "• " + LocationObj.Loc + " " + LocationObj.Res + "\n";
+    //         let LocSUIDArr = splitFunc(LocationObj.SUID);
+    //         let LocERArr = splitFunc(LocationObj.ER);
+    //         let LocLvArr = splitFunc(LocationObj.Lv);
+    //         for (let x = 0; x < LocSUIDArr.length; x++) {
+    //             if (LocSUIDArr[x] == Species.SUID) {
+    //                 Location.innerText += "Lv : " + LocLvArr[x] + " " + LocERArr[x] + "\n";
+    //             }
+    //         }
+    //     }
+    // } else {
+    //     Location.innerText += `• Unknown`;
+    // }
     
-    const SpeciesHeld = splitFunc(Species.Held);
-    const SpeciesChance = splitFunc(Species.Chance);
-    HeldItem.innerText = "Held Item : \n";
-    if (SpeciesHeld != "-") {
-        for (let x = 0; x < SpeciesHeld.length; x++) {
-            let HeldIndex = SpeciesHeld[x] - 60001;
-            let HeldObj = ItemsData[HeldIndex];
-            HeldItem.innerText += "• " + HeldObj.Name
-            + " " + SpeciesChance[x] + "\n";
-        }
-    } else {
-        HeldItem.innerText += `• None`;
-    }
+    // const SpeciesHeld = splitFunc(Species.Held);
+    // const SpeciesChance = splitFunc(Species.Chance);
+    // HeldItem.innerText = "Held Item : \n";
+    // if (SpeciesHeld != "-") {
+    //     for (let x = 0; x < SpeciesHeld.length; x++) {
+    //         let HeldIndex = SpeciesHeld[x] - 60001;
+    //         let HeldObj = ItemsData[HeldIndex];
+    //         HeldItem.innerText += "• " + HeldObj.Name
+    //         + " " + SpeciesChance[x] + "\n";
+    //     }
+    // } else {
+    //     HeldItem.innerText += `• None`;
+    // }
     
-    const SpeciesEggGroup = splitFunc(Species.Egg);
-    EggGroup.innerText = "Egg Group : \n";
-    for (let x = 0; x < SpeciesEggGroup.length; x++) {
-        EggGroup.innerText += "• " + SpeciesEggGroup[x] + "\n";
-    }
+    // const SpeciesEggGroup = splitFunc(Species.Egg);
+    // EggGroup.innerText = "Egg Group : \n";
+    // for (let x = 0; x < SpeciesEggGroup.length; x++) {
+    //     EggGroup.innerText += "• " + SpeciesEggGroup[x] + "\n";
+    // }
     
-    Evolution.innerHTML = "";
-    const EvoLine = splitFunc(Species.EvoLine);
-    const EvoText = document.createElement("div");
-    EvoText.innerText = "Evolution Chart : \n";
-    EvoText.setAttribute("class", "EvoTextBox")
-    Evolution.appendChild(EvoText)
-    if (EvoLine.length > 1) {
-        for (let x = 0; x < EvoLine.length; x++) {
-            if (EvoLine[x] == "0") {
-                continue;
-            }
-            let EvoContainer = document.createElement("div");
-            const ArrowBox = document.createElement("div");
-            ArrowBox.innerText = "↓";
-            ArrowBox.setAttribute("class", "ArrowBox")
+    // Evolution.innerHTML = "";
+    // const EvoLine = splitFunc(Species.EvoLine);
+    // const EvoText = document.createElement("div");
+    // EvoText.innerText = "Evolution Chart : \n";
+    // EvoText.setAttribute("class", "EvoTextBox")
+    // Evolution.appendChild(EvoText)
+    // if (EvoLine.length > 1) {
+    //     for (let x = 0; x < EvoLine.length; x++) {
+    //         if (EvoLine[x] == "0") {
+    //             continue;
+    //         }
+    //         let EvoContainer = document.createElement("div");
+    //         const ArrowBox = document.createElement("div");
+    //         ArrowBox.innerText = "↓";
+    //         ArrowBox.setAttribute("class", "ArrowBox")
             
-            let ConditionBox = document.createElement("div");
-            ConditionBox.setAttribute("class", "ConditionBox")
-            let Condition = SpeciesData[EvoLine[x]-1].Condition
-            let SpeciesCondition = splitFunc(Condition);
+    //         let ConditionBox = document.createElement("div");
+    //         ConditionBox.setAttribute("class", "ConditionBox")
+    //         let Condition = SpeciesData[EvoLine[x]-1].Condition
+    //         let SpeciesCondition = splitFunc(Condition);
             
-            if (SpeciesCondition.length == 1) {
-                if (+SpeciesCondition[0]) {
-                    ConditionBox.innerText = "(Level " + SpeciesCondition + ")";
-                } else {
-                    ConditionBox.innerText = "(Lv-Up with high Friendship)";
-                }
-            } else if (Ability.toString().includes(';')) {
-                let x = Condition.split("; ");
-                let y = splitFunc(x[1]);
-                ConditionBox.innerText = "(Level " + x[0] + " or ";
-                if (y.length == 1) {
-                    ConditionBox.innerText += "Lv-Up with " + x[1] + ")";
-                } else {
-                    ConditionBox.innerText += "Lv-Up while knowing move " + y[1] + ")";
-                }
-            } else if (SpeciesCondition.length == 2) {
-                if (SpeciesCondition[0] == "Mega") {
-                    ConditionBox.innerText = "(Mega evolve with " + SpeciesCondition[1] + ")";
-                } else if (SpeciesCondition[0] == "Item") {
-                    ConditionBox.innerText = "(Use " + SpeciesCondition[1] + ")";
-                    if (SpeciesCondition[1] == "Necrozium Z") {
-                        ConditionBox.innerText = "(Beastification with Necrozium Z)"
-                    }
-                } else if (SpeciesCondition[0] == "Move") {
-                    ConditionBox.innerText = "(Lv-Up while knowing move " + SpeciesCondition[1] + ")";
-                } else if (SpeciesCondition[0] == "Friendship") {
-                    if (SpeciesCondition[1] == "Fairy Type") {
-                        ConditionBox.innerText = "(Lv-Up with high Friendship while knowing any " + SpeciesCondition[1] + " move)";
-                    } else {
-                        ConditionBox.innerText = "(Lv-Up with high Friendship during " + SpeciesCondition[1] + " time)";
-                    }
-                } else if (+SpeciesCondition[0]) {
-                    if (SpeciesCondition[1] == "Day" || SpeciesCondition[1] == "Night"
-                    || SpeciesCondition[1] == "Dusk") {
-                        ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " during " + SpeciesCondition[1] + " time)";
-                    } else if (SpeciesCondition[1] == "Male" || SpeciesCondition[1] == "Female") {
-                        ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " " + SpeciesCondition[1] + " only)";
-                    } else if (SpeciesCondition[1] == "N1") {
-                        ConditionBox.innerText = "(Level " + SpeciesCondition[0] + "with " + N1;
-                    } else if (SpeciesCondition[1] == "N2") {
-                        ConditionBox.innerText = "(Level " + SpeciesCondition[0] + "with " + N2;
-                    } else {
-                        ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " with " + SpeciesCondition[1] + ")";
-                    }
-                } else if (SpeciesCondition[0] == "Necrozmizer") {
-                    ConditionBox.innerText = "(Use Necrozmizer on Necrozma and " + SpeciesCondition[1] + ")";
-                }
-            } else if (SpeciesCondition.length == 3) {
-                ConditionBox.innerText = "(Use " + SpeciesCondition[2] + " " + SpeciesCondition[1] + " only)";
-            }
+    //         if (SpeciesCondition.length == 1) {
+    //             if (+SpeciesCondition[0]) {
+    //                 ConditionBox.innerText = "(Level " + SpeciesCondition + ")";
+    //             } else {
+    //                 ConditionBox.innerText = "(Lv-Up with high Friendship)";
+    //             }
+    //         } else if (Ability.toString().includes(';')) {
+    //             let x = Condition.split("; ");
+    //             let y = splitFunc(x[1]);
+    //             ConditionBox.innerText = "(Level " + x[0] + " or ";
+    //             if (y.length == 1) {
+    //                 ConditionBox.innerText += "Lv-Up with " + x[1] + ")";
+    //             } else {
+    //                 ConditionBox.innerText += "Lv-Up while knowing move " + y[1] + ")";
+    //             }
+    //         } else if (SpeciesCondition.length == 2) {
+    //             if (SpeciesCondition[0] == "Mega") {
+    //                 ConditionBox.innerText = "(Mega evolve with " + SpeciesCondition[1] + ")";
+    //             } else if (SpeciesCondition[0] == "Item") {
+    //                 ConditionBox.innerText = "(Use " + SpeciesCondition[1] + ")";
+    //                 if (SpeciesCondition[1] == "Necrozium Z") {
+    //                     ConditionBox.innerText = "(Beastification with Necrozium Z)"
+    //                 }
+    //             } else if (SpeciesCondition[0] == "Move") {
+    //                 ConditionBox.innerText = "(Lv-Up while knowing move " + SpeciesCondition[1] + ")";
+    //             } else if (SpeciesCondition[0] == "Friendship") {
+    //                 if (SpeciesCondition[1] == "Fairy Type") {
+    //                     ConditionBox.innerText = "(Lv-Up with high Friendship while knowing any " + SpeciesCondition[1] + " move)";
+    //                 } else {
+    //                     ConditionBox.innerText = "(Lv-Up with high Friendship during " + SpeciesCondition[1] + " time)";
+    //                 }
+    //             } else if (+SpeciesCondition[0]) {
+    //                 if (SpeciesCondition[1] == "Day" || SpeciesCondition[1] == "Night"
+    //                 || SpeciesCondition[1] == "Dusk") {
+    //                     ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " during " + SpeciesCondition[1] + " time)";
+    //                 } else if (SpeciesCondition[1] == "Male" || SpeciesCondition[1] == "Female") {
+    //                     ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " " + SpeciesCondition[1] + " only)";
+    //                 } else if (SpeciesCondition[1] == "N1") {
+    //                     ConditionBox.innerText = "(Level " + SpeciesCondition[0] + "with " + N1;
+    //                 } else if (SpeciesCondition[1] == "N2") {
+    //                     ConditionBox.innerText = "(Level " + SpeciesCondition[0] + "with " + N2;
+    //                 } else {
+    //                     ConditionBox.innerText = "(Level " + SpeciesCondition[0] + " with " + SpeciesCondition[1] + ")";
+    //                 }
+    //             } else if (SpeciesCondition[0] == "Necrozmizer") {
+    //                 ConditionBox.innerText = "(Use Necrozmizer on Necrozma and " + SpeciesCondition[1] + ")";
+    //             }
+    //         } else if (SpeciesCondition.length == 3) {
+    //             ConditionBox.innerText = "(Use " + SpeciesCondition[2] + " " + SpeciesCondition[1] + " only)";
+    //         }
             
-            if (x != 0 && EvoLine[x - 1] != 0) {
-                EvoContainer.appendChild(ArrowBox)
-                EvoContainer.appendChild(ConditionBox);
-            }
+    //         if (x != 0 && EvoLine[x - 1] != 0) {
+    //             EvoContainer.appendChild(ArrowBox)
+    //             EvoContainer.appendChild(ConditionBox);
+    //         }
             
-            let EvoBox = CreateEvoBox(EvoLine[x]);
-            EvoContainer.appendChild(EvoBox)
+    //         let EvoBox = CreateEvoBox(EvoLine[x]);
+    //         EvoContainer.appendChild(EvoBox)
             
-            EvoContainer.setAttribute("class", "EvoContainer");
-            if (EvoLine.length-1 > 3 && x > 2) {
-                EvoContainer.setAttribute("class", "EvoContainerS" + (x-2));
-                //Style split 3 evo
-            } else if (x == 5) {
-                EvoContainer.setAttribute("class", "EvoContainerS3");
-                //Style split 2 evo with evo
-            } else if (x > 2 && EvoLine.length > 5) {
-                EvoContainer.setAttribute("class", "EvoContainer" + (x+2));
-            }
+    //         EvoContainer.setAttribute("class", "EvoContainer");
+    //         if (EvoLine.length-1 > 3 && x > 2) {
+    //             EvoContainer.setAttribute("class", "EvoContainerS" + (x-2));
+    //             //Style split 3 evo
+    //         } else if (x == 5) {
+    //             EvoContainer.setAttribute("class", "EvoContainerS3");
+    //             //Style split 2 evo with evo
+    //         } else if (x > 2 && EvoLine.length > 5) {
+    //             EvoContainer.setAttribute("class", "EvoContainer" + (x+2));
+    //         }
             
-            Evolution.appendChild(EvoContainer)
-        }
-    } else {
-        EvoText.innerText += Species.Name + " does not evolve.";
-    }
+    //         Evolution.appendChild(EvoContainer)
+    //     }
+    // } else {
+    //     EvoText.innerText += Species.Name + " does not evolve.";
+    // }
     
-    RenderLearnsetTable(Species.UID, Species.Name);
+    // RenderLearnsetTable(Species.UID, Species.Name);
     scrollTo(0, 0);
     //console.log("Time until Detail Rendered: ", Date.now()-timerStart);
 }
